5922e1057f06662a6d0b1a25c196538fb7556310
Bilal Kiani <bilalkiani.dev@gmail.com>
2025-11-08 10:57:05 +0500

    updated

--- file_io.py
+++
@@ -1,5 +0,0 @@
-#python can be used to perform operation on file in computer (read and write)
-#files can be of different formate:
-#Text files: .txt   .docx    .log etc
-#Binary files: .mp4    .mov     .png    .jpeg   etc
-
